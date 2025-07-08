5f1f49579cd528fe307862a0b6656656b8c26617
Krishna <kg9977553311@gmail.com>
2025-07-08 18:12:56 +0530

    Added Analytics

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,10 +3,13 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import ScrollToTop from '../components/ScrollToTop';
 import { Outlet } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/next';
+
 
 export default function MainLayout() {
   return (
     <>
+      <Analytics />
       <Navbar />
       <ScrollToTop />
       <Outlet />
